cddf562d4c191d24238f757a841aaa96d875aace
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-02-25 12:12:17 -0300

    renderizando personagem

--- script.js
+++ script.js
@@ -16,9 +16,23 @@ const fetchApi = (id) => { // o fetch pega os daddos da api e da a rsposta then
     return result;
 }
 
+const array = ['name', 'status', 'species', 'gender', 'origin', 'image', 'episode' ];
+
+const resultado = (result) => {
+    const newObjeto = {};
+    array.map((key) => document.getElementById(key))// cria novo array 
+    .map((elementos) => {
+        elementos.checked && (newObjeto[elementos.name] = result[elementos.name]);// faz um map nos que foi selecionado e cria um novo objeto so com os que foram checados
+    })
+    return newObjeto;
+}
+
 btnOk.addEventListener('click', async  (event) =>{
     event.preventDefault();// pega o id digitado  e faz o evento
     const resultadoApi = await fetchApi(characterId.value) // pega o personagem pelo id
-    content.textContent = `${JSON.stringify(resultadoApi, undefined, 2)}`;// traz um objeto
+   // content.textContent = `${JSON.stringify(resultadoApi, undefined, 2)}`;// traz um objeto
+    content.textContent = `${JSON.stringify(resultado(resultadoApi), undefined, 2)}`;// traz um objeto
+
+
     imagem.src = `${resultadoApi.image}`
 })
