bf4104cdf2c389948c66b3167e7b1a15367ff87b
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-02-28 14:28:43 -0300

    aray

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const btnOk = document.getElementById('btn-go');// start a função
 const content = document.getElementById('content');// para aparece o objeto
 const imagem = document.getElementById('img');
 const resetar = document.getElementById('btn-reset');
-const children = document.getElementById('content') 
+const children = document.getElementById('styled') 
 
 
 const fetchApi = (id) => { // o fetch pega os daddos da api e da a rsposta then
@@ -23,10 +23,15 @@ const array = ['name', 'status', 'species', 'gender', 'origin', 'episode' ];
 const resultado = (result) => {
     return array.map((key) => document.getElementById(key))// cria novo array 
     .map((elementos) => {
+        if(elementos.checked && Array.isArray(result[elementos.name]) === true ){
+            const newElement = document.createElement('p');
+            newElement.innerHTML = ` ${elementos.name}  : ${result[elementos.name]}`;
+            content.appendChild(newElement);
+        }else 
         if(elementos.checked && typeof(result[elementos.name]) !== 'object' ){
             const newElement = document.createElement('p');
             newElement.innerHTML = ` ${elementos.name}  : ${result[elementos.name]}`;
-            children.appendChild(newElement);
+            content.appendChild(newElement);
         }
     })
     
@@ -41,9 +46,16 @@ btnOk.addEventListener('click', async  (event) =>{
         return content.innerHTML= "Digite algun numero"
     }
     const resultadoApi = await fetchApi(characterId.value) // pega o personagem pelo id
-   // content.textContent = `${JSON.stringify(resultadoApi, undefined, 2)}`;// traz um objeto
-   // content.textContent = `${JSON.stringify(resultado(resultadoApi), undefined, 2)}`;// traz um objeto
-  resultado(resultadoApi);
+  
+    if (content.firstChild === null) {// primeiro filho para que não repita
+    children.className= 'styled'
+        imagem.src = `${resultadoApi.image}`// se tiver filho
+        resultado(resultadoApi);
+    } else {
+        content.innerHTML = '';// se ja tiver personagem selecionado não cira novamente
+          children.className = 'styled'
+        imagem.src = `${resultadoApi.image}`
+        resultado(resultadoApi);
+    }
 
-    imagem.src = `${resultadoApi.image}`
 })
